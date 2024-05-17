47f0afb6c20b7fc77ead87cfd30ce7526fe2045b
Pedro Lucas Sousa <pedrocsgo1020@hotmail.com>
2024-05-17 10:18:34 -0300

    add

--- TelaLogin/obj/Debug/net8.0-windows/TelaLogin.AssemblyInfo.cs
+++ TelaLogin/obj/Debug/net8.0-windows/TelaLogin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelaLogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5e72a3bd040edf31e8ebcf8f19c996e37ef8e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603d407e869d96f3b2ae3b492f2df9f0f8291c27")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelaLogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelaLogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
