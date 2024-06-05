7cccbe3b36e17071c9a506d7ce88c8b7c8fce113
Pedro Lucas Sousa <pedrocsgo1020@hotmail.com>
2024-06-04 22:37:23 -0300

    itens fornecidos 100%

--- TelaLogin/ClassGlobal/VarGlobal.cs
+++ TelaLogin/ClassGlobal/VarGlobal.cs
@@ -11,6 +11,5 @@ namespace TelaLogin.ClassGlobal
         // armazena o id do fornecedor
         public static int id { get; set; }
         public static string nome { get; set; }
-        public static int idItem { get; set; }
     }
 }
